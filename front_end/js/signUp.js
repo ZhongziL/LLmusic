dbf15c8215021e92c7550992e7de069c73c480c5
zhongziL <liangzzh@mail2.sysu.edu.cn>
2018-12-24 11:14:10 +0800

    signIn and signUp

--- front_end/js/signUp.js
+++ front_end/js/signUp.js
@@ -4,6 +4,23 @@ window.onload = function() {
 	});
 	
 	mui('.mui-content').on('click', '.mui-btn', function() {
-		alert('?');
+		var username = 'Liu';
+		var password = '123456';
+
+		var post_url = 'http://172.18.160.110:5000/register';
+		$.post(post_url,
+			{ // 数据
+				username: username,
+				password: password,
+			},
+			function (data, textStatus) {
+				if (textStatus === "success") { // 登录或注册成功
+					// document.cookie = data.split(";")[0];
+					alert(textStatus);
+				} else {    // 登录失败
+					alert(textStatus);
+				}
+			}
+		);
 	});
-}
+};
